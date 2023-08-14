5d04911870d38aef82986d5b980fefe6913d962c
udqps <udqps@student.kit.edu>
2023-08-14 11:29:58 +0200

    some changes to tscondig

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
-export default {
-    preset: 'ts-jest/presets/js-with-ts',
-    testEnvironment: 'node',
-    transformIgnorePatterns: [],
-  }
+module.exports = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'node',
+  transformIgnorePatterns: [],
+}
