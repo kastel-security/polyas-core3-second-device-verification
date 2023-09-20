451d747a852a84e6cf5228a0eb416e6588910176
udqps <udqps@student.kit.edu>
2023-09-20 14:42:17 +0200

    Polyas as env variable

--- vite.config.js
+++ vite.config.js
@@ -2,38 +2,42 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path='https://election.polyas.com/bcd5805a-3d1a-4bce-b958-4da9ca80254f/ssd/rest/'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: true,
-    port: 4300, // This is the port which we will use in docker
-    // Thanks @sergiomoura for the window fix
-    // add the next lines if you're using windows and hot reload doesn't work
-     watch: {
-       usePolling: true
-     },
-     proxy: {
-      '/electionData': {
-        target: path+'electionData',
-        changeOrigin: true
-      },
-      '/login': {
-        target: path+'login',
-        changeOrigin: true
-      },
-      '/challenge': {
-        target: path+'challenge',
-        changeOrigin: true
+import { loadEnv } from 'vite';
+
+
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  const path=process.env.VITE_POLYAS
+  return defineConfig({
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-     }
-  }
-})
+    },
+    server: {
+      host: true,
+      port: 4300,
+       watch: {
+         usePolling: true
+       },
+       proxy: {
+        '/electionData': {
+          target: path+'/electionData',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
+        '/login': {
+          target: path+'/login',
+          changeOrigin: true
+        },
+        '/challenge': {
+          target: path+'/challenge',
+          changeOrigin: true
+        }
+       }
+    }
+  });
+}
