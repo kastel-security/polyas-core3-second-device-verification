5e28597c3819b5edc892b027cb1691d2f1aa44b1
udqps <udqps@student.kit.edu>
2023-09-25 11:12:59 +0200

    Added dev mode with proxy

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,56 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 
 export default ({ mode }) => {
-  const deployConfig = {
-    plugins: [
-      vue(),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    server: {
-      host: true,
-      port: 5000
-    }
-  }
-  const devConfig = {
-    plugins: [
-      vue(),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
-    server: {
-      host: true,
-      port: 5000,
-      
-      proxy: {
-        '/electionData': {
-          target: 'https://elections-k8s-dev.polyas.com/10f9e7ec-a92d-43c5-8262-22d73ffc57ce/ssd/rest',
-          changeOrigin: true
-        },
-        '/login': {
-          target: 'https://elections-k8s-dev.polyas.com/10f9e7ec-a92d-43c5-8262-22d73ffc57ce/ssd/rest',
-          changeOrigin: true
-        },
-        '/challenge': {
-          target: 'https://elections-k8s-dev.polyas.com/10f9e7ec-a92d-43c5-8262-22d73ffc57ce/ssd/rest',
-          changeOrigin: true
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  if (process.env.VITE_MODE == 'dev') {
+    return defineConfig({
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      server: {
+        host: true,
+        port: 5000,
+        
+        proxy: {
+          '/electionData': {
+            target: process.env.VITE_POLYAS + '/ssd/rest',
+            changeOrigin: true
+          },
+          '/login': {
+            target: process.env.VITE_POLYAS + '/ssd/rest',
+            changeOrigin: true
+          },
+          '/challenge': {
+            target: process.env.VITE_POLYAS + '/ssd/rest',
+            changeOrigin: true
+          }
         }
       }
-    }
+    })
+  } else {
+    return defineConfig({
+      plugins: [
+        vue(),
+      ],
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+      server: {
+        host: true,
+        port: 5000
+      }
+    });
   }
-  return defineConfig(devConfig);
 
 }
