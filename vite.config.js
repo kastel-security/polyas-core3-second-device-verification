273f237872b3d3bf733626c48ddbff1480af812c
udqps <udqps@student.kit.edu>
2023-09-25 10:55:22 +0200

    added proxy to server

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 
 export default ({ mode }) => {
-  return defineConfig({
+  const deployConfig = {
     plugins: [
       vue(),
     ],
@@ -18,5 +18,36 @@ export default ({ mode }) => {
       host: true,
       port: 5000
     }
-  });
+  }
+  const devConfig = {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: true,
+      port: 5000,
+      
+      proxy: {
+        '/electionData': {
+          target: 'https://elections-k8s-dev.polyas.com/10f9e7ec-a92d-43c5-8262-22d73ffc57ce/ssd/rest',
+          changeOrigin: true
+        },
+        '/login': {
+          target: 'https://elections-k8s-dev.polyas.com/10f9e7ec-a92d-43c5-8262-22d73ffc57ce/ssd/rest',
+          changeOrigin: true
+        },
+        '/challenge': {
+          target: 'https://elections-k8s-dev.polyas.com/10f9e7ec-a92d-43c5-8262-22d73ffc57ce/ssd/rest',
+          changeOrigin: true
+        }
+      }
+    }
+  }
+  return defineConfig(devConfig);
+
 }
