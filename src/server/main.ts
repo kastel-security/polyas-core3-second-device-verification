7d63846894f512119e6b9b4275d01a52200e52f3
udqps <udqps@student.kit.edu>
2023-09-26 12:42:03 +0200

    proxy works now, everything else doesnt

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,59 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import * as fs from 'fs';
+import jsPDF from "jspdf";
 import * as dotenv from "dotenv"
 
+type ValidEnding = "B"|"KB"|"MB"|"GB"|"TB"|"PB"
+const endings = {
+  "B": 0,
+  "KB": 3,
+  "MB": 6,
+  "GB": 9,
+  "TB": 12,
+  "PB": 15
+}
+
+function getBytes() {
+  const byteString = process.env.CAPACITY as string
+  const groups = byteString.match(/([0-9]+)([A-Z]*)/)
+  if (groups === null || groups.length < 3) {
+    return 0
+  }
+  const ending = groups[2] as ValidEnding
+  return Math.pow(10, endings[ending]) * (groups[1] as any as number)
+}
+
+function logIfNotFull() {
+  const path = process.env.VITE_HASH as string
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path)
+  }
+  const files = fs.readdirSync(path)
+  let totalSize = 0
+  files.forEach(function(file) {
+    totalSize += fs.statSync(path + "/" + file).size
+  })
+  const newFile = path + "/" + Math.random() + ".pdf" 
+  
+  if (totalSize < getBytes()) {
+    const doc = new jsPDF();
+
+    doc.text("Hello world!", 10, 10);
+    doc.save(newFile);
+  }
+  
+  return totalSize
+
+}
 
 const app = express();
 app.use(express.json()); // <--- Here
 app.use(express.urlencoded({extended: true}));
 dotenv.config()
 app.post("/calc", function(req, res) {
-  res.send("size:" + req.body)
+  const size = logIfNotFull()
+  res.send("size:" + req.body.num1)
 });
 
 app.get("/log", (_, res) => {
@@ -16,5 +61,5 @@ app.get("/log", (_, res) => {
 })
 
 ViteExpress.listen(app, 5000, () =>
-  console.log("Server is listening on port 5000...")
+  console.log("Server is listening on port 3000...")
 );
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-
+// https://vitejs.dev/config/
 export default defineConfig({
-      plugins: [
-        vue(),
-      ],
-      server: {
-        host: true,
-        port: 5000,
-        
-        proxy: {
-          '/electionData': {
-            target: process.env.VITE_INSTANCE + '/ssd/rest',
-            changeOrigin: true
-          },
-          '/login': {
-            target: process.env.VITE_INSTANCE + '/ssd/rest',
-            changeOrigin: true
-          },
-          '/challenge': {
-            target: process.env.VITE_INSTANCE + '/ssd/rest',
-            changeOrigin: true
-          }
-        }
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 3000,
+    
+    proxy: {
+      '/electionData': {
+        target: "https://elections-k8s-dev.polyas.com/10f9e7ec-a92d-43c5-8262-22d73ffc57ce/ssd/rest",
+        changeOrigin: true
       }
-})
+    }
+  }
+});
