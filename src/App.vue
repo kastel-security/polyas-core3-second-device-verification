11ab33759589f3db79a59fcf16d874569f91e13d
Michael Kirsten <kirsten@kit.edu>
2023-09-28 06:16:18 +0200

    Copyright notice also on website

--- src/App.vue
+++ src/App.vue
@@ -147,6 +147,7 @@ async function reset (): Promise<void> {
     </div>
     <div id="footer">
       <a href="https://github.com/kastel-security/polyas-core3-second-device-verification">Polyas-Verifier</a> {{ extractTextFromJson(text.footer.acknowledgement, language) }}
+      &copy; 2023&puncsp;<a href="mailto:udqps@student.kit.edu">Christoph Niederbudde</a>, <a href="https://formal.kastel.kit.edu/~kirsten/">Michael Kirsten</a>.
     </div>
   </div>
 </template>
